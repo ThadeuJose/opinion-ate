40f43b116afe8ba8ef5647b512361409213aebf4
Thadeu José <thadeujs@gmail.com>
2021-06-24 21:35:52 -0300

    Create a restaurant full implemented
    Create a restaurant now is full implement with edge cases

--- src/components/RestaurantScreen.vue
+++ src/components/RestaurantScreen.vue
@@ -2,6 +2,7 @@
   <v-card>
     <v-card-title>Restaurants</v-card-title>
     <v-card-text>
+      <NewRestaurantForm />
       <RestaurantList />
     </v-card-text>
   </v-card>
@@ -9,8 +10,9 @@
 
 <script>
 import RestaurantList from '@/components/RestaurantList';
+import NewRestaurantForm from './NewRestaurantForm.vue';
 export default {
   name: 'RestaurantScreen',
-  components: {RestaurantList},
+  components: {RestaurantList, NewRestaurantForm},
 };
 </script>
